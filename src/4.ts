9467940c9ea1885129474dc42e77ad49b93aed3c
alina prysiazhniuk <alinkaprisiazhnuyk@gmail.com>
2024-04-09 20:05:45 +0300

    4

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,59 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+  getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+  public door = false; //true
+  protected key: Key;
+  private tenants: Person[] = [];
+
+  public comeIn(person: Person) {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log(
+        `Person with key signature ${person.getKey().getSignature()} came in.`
+      );
+    }
+  }
+
+  public abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super();
+    this.key = key;
+  }
+  public openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
-
 house.comeIn(person);
-
-
-export {};
